c8159f207404e4838361d886bcd154758a9f948a
Labo95 <labibhasan819@gmail.com>
2024-12-18 18:36:19 +0800

    email update

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -19,7 +19,7 @@ const Footer = () => {
                     <div className="mr-1 text-base">
                         <MdMail />
                     </div>
-                    <div>iosamajavaid@gmail.com</div>
+                    <div>labibhasan819@gmail.com</div>
                 </div>
             </div>
         </div>
